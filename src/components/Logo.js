6ddb9fcc956e75fdcc66acdbac43f0f6a9dde5bb
OlSavMe <olga_savka.os@mail.ru>
2020-06-22 23:03:49 +0300

    animated logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../styles/Logo.scss";
 
 import { Link } from "react-router-dom";
 
 const Logo = ({ open, setOpen }) => {
+  const prevPos = useRef(0);
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let scrollPos = window.scrollY;
+      if (scrollPos !== prevPos.current) {
+        setScroll(true);
+      }
+      if (scrollPos === 0) {
+        setScroll(false);
+      }
+
+      prevPos.current = scrollPos;
+      console.log(scroll, scrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scroll]);
+
   return (
-    <main>
+    <main className={scroll ? "scroll" : ""}>
       <Link className={open ? "logo-active" : "logo"} to="/">
         <img src={require("../assets/s.gif")} alt="logo"></img>
       </Link>
