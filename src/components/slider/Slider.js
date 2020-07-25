ba2f0b109a7bdfc3934b48c65d7f42d264d14b23
OlSavMe <olga_savka.os@mail.ru>
2020-07-25 16:51:13 +0300

    next button added to slider

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -10,27 +10,35 @@ const Slider = ({ slides }) => {
 
   // console.log(current);
 
+  // const toNext = () => {
+  //   setCurrent(current === length - 1 ? 0 : current + 1);
+  // };
+
   const toNext = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
+    const index = current === length - 1 ? 0 : current + 1;
+
+    // assign the logical index to current image index that will be used in render method
+    setCurrent(index);
   };
 
-  useEffect(() => {
-    setTimeout(toNext, 2000);
-    //clear timeout when component unmounts
-    return function () {
-      clearTimeout(toNext);
-    };
-  });
-  if (!Array.isArray(slides) || slides.length <= 0) {
-    return null;
-  }
+  // useEffect(() => {
+  //   // setTimeout(toNext, 3000);
+  //   // //clear timeout when component unmounts
+  //   // return function () {
+  //   //   clearTimeout(toNext);
+  //   // };
+  //   toNext();
+  // }, []);
+  // if (!Array.isArray(slides) || slides.length <= 0) {
+  //   return null;
+  // }
 
   return (
     <section className="slider">
       {slides.map((item, index) => (
         <div
           className={index === current ? "slide active" : "slide"}
-          key={item.id}
+          key={index}
           //for accessability reasons each non-active element is hidden
           aria-hidden={index !== current}
         >
@@ -44,6 +52,7 @@ const Slider = ({ slides }) => {
           )}
         </div>
       ))}
+      <button onClick={toNext}>Next</button>
     </section>
   );
 };
