9f3a1747417653468cd06462dfcbc2b06116149a
OlSavMe <olga_savka.os@mail.ru>
2020-08-19 21:50:16 +0300

    photo and nav styling

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -12,16 +12,21 @@ function Photo(props) {
         </figure>
       </section>
       <p>
-        Hi! I am a Web Developer living in Helsinki. I enjoy creating
-        eye-catching and user-friendly applications. I have hands-on experience
-        in app building using JavaScript, Reac.js (v16.8), React Native,
-        SASS/SCSS, JQuery, Bootstrap, REST API, headless CMS, Node.js, Express,
-        Google Cloud tools, SQL, CI/CD pipelines and UX prototyping. I've been
-        working in multidisciplinary Agile teams within several web development
+        Hi!{" "}
+        <span role="img" aria-label="grinning face">
+          😃
+        </span>{" "}
+        I am a Web Developer living in Helsinki. I enjoy creating eye-catching
+        and user-friendly applications. I have hands-on experience in app
+        building using JavaScript, Reac.js (v16.8), React Native, SASS/SCSS,
+        JQuery, Bootstrap, REST API, headless CMS, Node.js, Express, Google
+        Cloud tools, SQL, CI/CD pipelines and UX prototyping. I've been working
+        in multidisciplinary Agile teams within several web development
         projects. In 2020, I am graduating from Haaga Helia UAS as a Software
         Developer. Due to my previous Tech Translator’s career, I have
         substantial experience of international teamwork.
       </p>
+      <p></p>
     </div>
   );
 }
