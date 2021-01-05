3d00548ad53cac4ebabe85c39db33e9703bbf007
OlSavMe <olga_savka.os@mail.ru>
2021-01-05 21:18:19 +0200

    content update

--- src/components/References.js
+++ src/components/References.js
@@ -62,24 +62,18 @@ const References = () => {
             </li>
             <li className="anim4">
               <div className="quote">
-                When I was interviewing Olga for the developer position, I was
-                very impressed noticing how level headed and professional she
-                is. Olga is very practical with expectations setting and
-                delivers what's promised in due time even if it requires
-                learning new things on the go. She also led sessions at The
-                Shortcut hosted FreeCodeCamp meetup on Redux and did a
-                tremendous job as per feedback from the participants. She is
-                very proficient with JavaScript frontend technologies and
-                especially comfortable with React and necessary libraries to
-                pull off a project from scratch. Olga worked on The Shortcut's
-                database management system's frontend and the new website of The
-                Shortcut both of which required her to use React, and she did a
-                great thorough job on both. I would highly recommend Olga as a
-                great professional, friendly, and welcoming team player and a
-                developer who works very calmly under pressure.
+                Olga is a real professional! Working on the order, she not only
+                fulfilled everything that was ordered, but gave many useful
+                recommendations. She created our website very carefully and
+                neatly. Work on the website was carried out quickly and in
+                accordance with all requirements. Each stage was agreed and the
+                necessary improvements were discussed. The site not only meets
+                all the wishes, but is also very convenient for administration.
+                I can definitely recommend her to my colleagues! It's a pleasure
+                to work with Olga!
               </div>
               <div className="source">
-                - Mojjammil Khandker, CTO at The Shortcut
+                - Valeriia Shitsel, Owner at Alejandra Beauty Salon
               </div>
             </li>
             <li className="anim5">
